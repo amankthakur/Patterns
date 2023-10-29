e0cee485c802711adab1e84c94ed716eb376f5a4
Aman Kumar Thakur <113065155+amankthakur@users.noreply.github.com>
2023-10-30 03:52:32 +0530

    Add files via upload

---
+++ pattern.cpp
@@ -0,0 +1,472 @@
+#include <iostream>
+using namespace std;
+// int main()
+// {
+//     int n;
+//     cout<<"enter n \n";
+//     cin>>n;
+//     for(int i=0;i<=n;i++)
+//     {
+//         for(int j=0;j<i;j++)
+//         {
+//             cout<<i;
+//         }
+//         cout<<"\n";
+//     }
+// }
+
+// int main()
+// {
+//     int n;
+//     cout<<"enter n \n";
+//     cin>>n;
+//     int M=n;
+//     for(int i=0;i<n;i++)
+//     {
+//         for(int j=1;j<=n;j++)
+//         {
+//             cout<<M;
+//             M=M-1;
+//         }
+//         cout<<"\n";
+//         M=n;
+//     }
+// }
+
+// int main()
+// {
+//     int k=1;
+//     int n;
+//     cout<<"Enter n \n";
+//     cin>>n;
+//     for(int i=1;i<=n;i++)
+//     {
+//         for(int j=1;j<=n;j++)
+//         {
+//             cout<<k<<" ";
+//             k=k+1;
+//         }
+//         cout<<"\n";
+//     }
+// }
+
+// int main()
+// {
+//     int n;
+//     cout<<"Enter n \n";
+//     cin>>n;
+//     for(int i=1;i<=n;i++)
+//     {
+//         for(int j=1;j<=i;j++)
+//         {
+//             cout<<"*";
+//         }
+//         cout<<"\n";
+//     }
+// }
+
+// int main()
+// {
+//     int n;
+//     cout<<"Enter n \n";
+//     cin>>n;
+//     for(int i=1;i<=n;i++)
+//     {
+//         for(int j=1;j<=i;j++)
+//         {
+//             cout<<i;
+//         }
+//         cout<<"\n";
+//     }
+// }
+
+// int main()
+// {   int i=1;
+//     int k=i;
+//     int n;
+//     cout<<"Enter n \n";
+//     cin>>n;
+//     for(;i<=n;i++)
+//     {
+//         for(int j=1;j<=i;j++)
+//         {
+//             cout<<k;
+//             k=i+j;
+//         }
+//         cout<<"\n";
+//         k=i+1;
+//     }
+// }
+
+// int main()
+// {
+//     int k;
+//     int n;
+//     cout << "Enter n \n";
+//     cin >> n;
+//     for (int i = 1; i <= n; i++)
+//     {
+//         k = i;
+//         for (int j = 1; j <= i; j++)
+//         {
+//             cout << k;
+//             k = k - 1;
+//         }
+//         cout << "\n";
+//     }
+// }
+
+// int main()
+// {
+//     int k=1;
+//     int n;
+//     cout<<"Enter n \n";
+//     cin>>n;
+//     for(int i=0;i<=n-1;i++)
+//     {
+//         for(int j=1;j<=n;j++)
+//         {
+//             char ch='A'+i;
+//             cout<<ch;
+//         }
+//         cout<<"\n";
+//     }
+// }
+
+// int main()
+// {
+//     int k=1;
+//     int n;
+//     cout<<"Enter n \n";
+//     cin>>n;
+//     for(int i=0;i<=n-1;i++)
+//     {
+//         for(int j=0;j<n;j++)
+//         {
+//             char ch='A'+j;
+//             cout<<ch;
+//         }
+//         cout<<"\n";
+//     }
+// }
+
+// int main()
+// {
+//     char ch='A';
+//     int n;
+//     cout<<"Enter n \n";
+//     cin>>n;
+//     for(int i=0;i<=n-1;i++)
+//     {
+//         for(int j=1;j<=n;j++)
+//         {
+//             cout<<ch;
+//             ch=ch+1;
+//         }
+//         cout<<"\n";
+//     }
+// }
+
+// int main()
+// {
+//     char ch='A';
+//     int n;
+//     cout<<"Enter n \n";
+//     cin>>n;
+//     for(int i=0;i<=n-1;i++)
+//     {
+//         for(int j=1;j<=n;j++)
+//         {
+//             cout<<ch;
+//             ch=ch+1;
+//         }
+//         cout<<"\n";
+//         ch=ch-1;
+//     }
+// }
+
+// int main()
+// {
+//     char ch='A';
+//     int n;
+//     cout<<"Enter n \n";
+//     cin>>n;
+//     for(int i=0;i<=n-1;i++)
+//     {
+//         for(int j=1;j<=n;j++)
+//         {
+//             cout<<ch;
+//             ch=ch+1;
+//         }
+//         cout<<"\n";
+//         ch='A'+1+i;
+//     }
+// }
+
+// int main()
+// {
+//     int k=0;
+//     int n;
+//     cout<<"Enter n \n";
+//     cin>>n;
+//     for(int i=1;i<=n;i++)
+//     {
+//         for(int j=1;j<=i;j++)
+//         {
+//             char ch='A'+k;
+//             cout<<ch;
+//         }
+//         cout<<"\n";
+//         k++;
+//     }
+// }
+
+// int main()
+// {
+//     int k=1;
+//     int n;
+//     cout<<"Enter n \n";
+//     cin>>n;
+//     for(int i=0;i<=n-1;i++)
+//     {
+//         for(int j=1;j<=n;j++)
+//         {
+//             char ch='A'+i;
+//             cout<<ch;
+//         }
+//         cout<<"\n";
+//     }
+// }
+
+// int main()
+// {
+//     int n;
+//     cout << "Enter n \n";
+//     cin >> n;
+//     for (int i = 0; i <= n; i++)
+//     {
+//         char ch = ('A' + (n - i));
+
+//         for (int j = 0; j < i; j++)
+//         {
+//             cout << ch;
+//             ch=ch+1;
+//         }
+//         cout << "\n";
+//     }
+// }
+
+// int main()
+// {
+//     int n;
+//     cout << "Enter n \n";
+//     cin >> n;
+//     for (int i = 1; i <=n; i++)
+//     {
+//         int space = n - i;
+//         for (int k = 0; k < space; k++)
+//         {
+//             cout << " ";
+//         }
+//         for (int t = 0; t < i; t++)
+//         {
+//             cout<<"*";
+//         }
+//         cout<<"\n";
+//     }
+// }
+
+// int main()
+// {
+//     int n;
+//     cout << "Enter n \n";
+//     cin >> n;
+//     int s=n;
+//     for (int i = 1; i <=n; i++)
+//     {
+//         for (int k = 1; k <=s; k++)
+//         {
+//             cout << "*";
+//         }
+//         cout<<"\n";
+//         s--;
+//     }
+// }
+
+// int main()
+// {
+//     int n;
+//     cout << "Enter n \n";
+//     cin >> n;
+//     int s=n;
+//     for (int i = 1; i <=n; i++)
+//     {
+
+//         for (int k = 1; k <=s; k++)
+//         {
+//             cout << "*";
+//         }
+//         cout<<"\n";
+//         s--;
+//         for(int t=0;t<i;t++)
+//         {
+//             cout<<" ";
+//         }
+//     }
+// }
+
+// int main()
+// {
+//     int n;
+//     cout << "Enter n \n";
+//     cin >> n;
+//     int s=n;
+//     for (int i = 1; i <=n; i++)
+//     {
+
+//         for (int k = 1; k <=s; k++)
+//         {
+//             cout << i;
+//         }
+//         cout<<"\n";
+//         s--;
+//         for(int t=0;t<i;t++)
+//         {
+//             cout<<" ";
+//         }
+//     }
+// }
+
+// int main()
+// {
+//     int n;
+//     cout << "Enter n \n";
+//     cin >> n;
+//     int s=n;
+//     for (int i = 1; i <=n; i++)
+//     {
+//         for (int k = 1; k <=s; k++)
+//         {
+//             cout << " ";
+//         }
+//         s--;
+//         for(int t=0;t<i;t++)
+//         {
+//             cout<<i;
+//         }
+//         cout<<"\n";
+//     }
+// }
+
+
+// int main()
+// {
+//     int n;
+//     cout << "Enter n \n";
+//     cin >> n;
+//     int count=1;
+//     int s=n;
+//     for (int i = 1; i <=n; i++)
+//     {
+
+//         for (int k = count; k <=n; k++)
+//         {
+//             cout << k;
+//         }
+//         count++;
+//         cout<<"\n";
+       
+//         for(int t=0;t<i;t++)
+//         {
+//             cout<<" ";
+//         }
+//     }
+// }
+
+// int main()
+// {
+//     int n;
+//     cout << "Enter n \n";
+//     cin >> n;
+//     int count=1; 
+//     int s=n; 
+//     for (int i = 1; i <=n; i++)
+//     {
+
+//         for (int k = 1; k <s; k++)
+//         {
+//             cout<<" ";
+//         }      
+//         for(int t=0;t<i;t++)
+//         {
+//             cout<<count;
+//             count++;
+//         }
+//         cout<<"\n";
+//         s--;
+//     }
+// }
+
+// Pyramid
+// int main()
+// {
+//     int n;
+//     cout << "Enter n \n";
+//     cin >> n;
+//     int x=n;
+//     for(int i=1;i<=n;i++)
+//     {
+//         int count=1;
+//         for(int j=1;j<x;j++)
+//         {
+//             cout<<" ";
+//         }
+//         for(int p=1;p<=i;p++)
+//         {
+//             cout<<count;
+//             count++;
+//         }
+//         for(int start=i-1;start>0;start--)
+//         {
+//             cout<<start;
+//         }
+//         x--;
+//         cout<<"\n";
+//     }
+// }
+
+// Dabangg Pattern
+//  on pYramid
+// int main()
+// {
+//     cout << endl;
+//     cout << "You are going to see dabangg pattern:- " << endl;
+
+//     int height, i, j, k, l;
+//     cout << endl;
+//     cout << "Enter height of the pattern:- ";
+//     cout << endl;
+//     cin >> height;
+//     cout << endl;
+
+//     for (i = 1; i <= height; i++)
+//     {
+//         for (j = 1; j <= (height - i + 1); j++)
+//         {
+//             cout << j << " ";
+//         }
+//         for (k = 1; k <= (i - 1); k++)
+//         {
+//             cout << "* ";
+//         }
+//         for (k = (i - 1); k >= 1; k--)
+//         {
+//             cout << "* ";
+//         }
+//         for (l = (height - i + 1); l >= 1; l--)
+//         {
+//             cout << l << " ";
+//         }
+//         cout << endl;
+//     }
+// }
